2cd67f7c192ff53c28b4c7d92a9302d76ae26e62
alex-designlogica <148463732+alex-designlogica@users.noreply.github.com>
2025-06-07 16:15:29 +0100

    added moves page

---
+++ src/main/java/net/ajsdev/cobblemonbookwiki/book/page/MovesPage.java
@@ -0,0 +1,17 @@
+package net.ajsdev.cobblemonbookwiki.book.page;
+
+import com.cobblemon.mod.common.pokemon.FormData;
+import com.cobblemon.mod.common.pokemon.Species;
+import net.minecraft.network.chat.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovesPage {
+    public static List<Component> build(FormData formData, Species species) {
+
+
+        List<Component> pages = new ArrayList<>();
+        return pages;
+    }
+}
